c4322086d65beb026b12c370d8bd62e5d661776c
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-03-06 10:31:50 -0600

    Add files via upload

---
+++ despensa.py
@@ -0,0 +1,38 @@
+import csv  # Librería para abrir, leer y escribir archivos CSV
+
+
+class Despensa:  # Clase Despensa
+
+    def __init__(self): # Constructor de la clase
+        pass # Inicializa el objeto
+
+    def listarDespensa(self, fecha: str) -> bool: # Metodo para listarDespensa()
+        # TODO programar el método listarDespensa()
+        # mostrar los registros por la fecha indicada con los siguientes datos:
+        # sku, nombre del producto, nombre de la tienda
+        return False # Regresa False si ocurrio un error en el metodo # Regresa False si ocurrio un error en el metodo
+
+    def insertarDespensa(self) -> bool: # Metodo para insertarDespesa()
+        # TODO programar el método insertarDespensa()
+        return False # Regresa False si ocurrio un error en el metodo
+
+    def buscarDespensa(self,id:int) -> None: # Metodo para buscarDespesa()
+        # TODO programar el método buscarDespensa()
+        # buscar despensa por id
+        return False # Regresa False si ocurrio un error en el metodo
+
+    def borrarDespensa(self,id:int) -> None: # Metodo para borrarDespensa()
+        # TODO programar el método borrarDespensa()
+        return False # Regresa False si ocurrio un error en el metodo
+
+    def actualizarDespensa(self) -> None: # Metodo para actualizarDespensa()
+        # TODO programar el método actualizarDespensa()
+        return False # Regresa False si ocurrio un error en el metodo
+
+    def valorMinimoProducto(self,sku:str) -> None: # Metodo para mostrar el valor minimo de un producto
+        # TODO programar el método valorMinimoProducto()
+        return False # Regresa False si ocurrio un error en el metodo
+
+    def valorMaximoProducto(self,sku:str) -> None: # Metodo para mostrar el valor minimo de un producto
+        # TODO programar el método valorMinimoProducto()
+        return False # Regresa False si ocurrio un error en el metodo
---
+++ main.py
@@ -0,0 +1,43 @@
+from productos import Productos # Importa el modulo Productos
+from tiendas import Tiendas # Importa el modulo Tiendas
+#TODO importar Despensa
+
+class Main(): # Clase principal
+
+    def __init__(self) -> None: # Constructor de la clase Main
+        pass # inicializa el objeto Main
+
+    def menu(self): # Metodo que muestra el menu del sistema
+        productos = Productos() # Crea un objeto de la clase Prodcutos
+        while True: # Bucle infinito para mostrar las opciones del sistema
+            print("0.- Listar productos") # Opcion para listar los prodcutos
+            print("1.- Insertar producto") # Opcion para insertar un nuevo producto
+            print("2.- Buscar producto por SKU") # Opcion para buscar productos por SKU
+            print("3.- Actualizar producto") # Opcion para actualizar un producto
+            print("4.- Borrar producto") # Opcion para borrar un producto
+            print("s.- Salir") # Opcion para salir del sistema
+            opcion = input("Seleccione una opcion: ") # Solicita al usuario que seleccione una opcion del menu
+            if opcion == "0": # Valida si la opcion elegida es el 0
+                productos.listarProductos() # Llama al metodo listarProductos a traves del objeto productos
+            elif opcion == "1": # Valida si la opcion elegida es el 1
+                # TODO Llamar al metodo insertarProducto()
+                print("Llamar metodo insertarProducto()")
+            elif opcion == "2": # Valida si la opcion elegida es el 2
+                # TODO Llamar al metodo buscarProducto()
+                print("Llamar metodo buscarProducto()")
+            elif opcion == "3": # Valida si la opcion elegida es el 3
+                # TODO Llamar al metodo actualizarProducto()
+                print("Llamar metodo actualizarProducto()")
+            elif opcion == "4": # Valida si la opcion elegida es el 4
+                # TODO Llamar al metodo borrarProducto()
+                print("Llamar metodo borrarProducto()")
+            elif opcion == "5": # Valida si la opcion elegida es el 5
+                # TODO Programar salir del programa
+                print("Salir del programa")
+
+            # TODO rediseñar el menu para agregar los submenus necesarios
+
+
+if __name__ == "__main__": # Define el modulo principal
+    principal = Main() # Crea un objeto de la clase Main
+    principal.menu() # Llama al metodo menu a traves del objeto principal

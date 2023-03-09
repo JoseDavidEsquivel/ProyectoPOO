1b1359fd3405d73cfa1c67a3d491580f5fea1abc
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-03-09 13:40:45 -0600

    Update nuevo.py
    Agregacion de todo el codigo

--- temporal/nuevo.py
+++ temporal/nuevo.py
@@ -1,1 +1,22 @@
+class Productos():
 
+    def listarProductos(self) -> bool:
+        return False
+
+    def insertarProducto(self, sku:str, nombre:str, unidad:str) -> bool:
+        print(f"INSERTAR: SKU: {sku} ,Nombre: {nombre} , Unidad: {unidad}")
+        return True
+    def actualizarProducto(self) -> bool:
+        sku= input("SKU: ")
+        nombre = input("Nombre: ")
+        unidad = input("Unidad: ")
+        print(f"ACTUALIZAR: SKU: {sku} ,Nombre: {nombre} , Unidad: {unidad}")
+        return True
+
+productos = Productos()
+sku= input("SKU: ")
+nombre = input("Nombre: ")
+unidad = input("Unidad: ")
+
+productos.insertarProducto(sku,nombre,unidad)
+productos.actualizarProducto()

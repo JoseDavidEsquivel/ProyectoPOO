375e8483b0f70cc979ad2a0e70bac6f5e828482f
LIRA-MIGUEL <122810468+LIRA-MIGUEL@users.noreply.github.com>
2023-03-18 21:31:06 -0600

    se actualizo productos.py
    se agrego el codigo de borrarProductos

--- productos.py
+++ productos.py
@@ -28,9 +28,26 @@ class Productos:  # Clase Productos
         return False # Regresa False si ocurrio un error en el metodo
 
     def borrarProductos(self) -> bool: # Metodo para borrar un producto
-        # TODO programar el método borrarProducto()
-        print("borrarProductos") #Imprime borrarProductos
-        return False # Regresa False si ocurrio un error en el metodo
+       def borrarProducto(self, archivo_csv, sku): 
+        # define una función llamada borrar_producto que recibe el nombre del archivo, y el sku que es por el cual se va a borrar el producto 
+
+        with open(archivo_csv, 'r', newline='') as archivo, open('temp.csv', 'w', newline='') as archivo_temp:
+            # abre dos archivos en modo de lectura y escritura respectivamente, utilizando la función open() de Python.
+
+            lector_csv = csv.reader(archivo) 
+            # se crea un objeto csv.reader que permite iterar por cada fila del archivo CSV y se asigna a la variable lector_csv
+
+            escritor_csv = csv.writer(archivo_temp) 
+            # También se crea un objeto csv.writer que permite escribir en el archivo temporal y se asigna a la variable escritor_csv
+
+            for fila in lector_csv:
+                # si la fila no contiene el producto que deseas borrar, escribirla en el archivo de salida
+                if fila[0] != sku:
+                    escritor_csv.writerow(fila)
+
+        # reemplazar el archivo original con el archivo de salida
+        import os
+        os.replace('temp.csv', archivo_csv)
 
     def actualizarProductos(self) -> bool: # Metodo para actualizar un producto
         # TODO programar el método actualizarProducto()
@@ -40,4 +57,4 @@ productos = Productos() # Crea un objeto de la clase Productos
 productos.listarProductos() # Llama al metodo listarProductos()
 productos.insertarProductos() #Llama al metodo insertarProductos()
 productos.buscarProductos() # Llama al metodo buscarProductos()
-productos.borrarProductos() #Llama al metodo borrarProductos()
+productos.borrarProductos("productos.csv", "SKU123") # Llamar al método borrarProductos() con los parámetros necesarios

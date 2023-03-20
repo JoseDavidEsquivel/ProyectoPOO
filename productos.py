27f6548af766cfb999dbde451264f387438d97f9
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-03-19 18:15:57 -0600

    Update productos.py
    Agregacion del metodo "insertarProducto"

--- productos.py
+++ productos.py
@@ -18,9 +18,39 @@ class Productos:  # Clase Productos
             return False # Regresa False si ocurrio un error en el metodo
 
     def insertarProductos(self): # Metodo para insertar un producto
-        # TODO programar el método insertarProducto()
-        print("insertarProducto") # Imprime insertarProductos
-        return False # Regresa False si ocurrio un error en el metodo
+        try:
+            with open('productos.csv', 'a', newline='') as file:
+        
+                # Crear un objeto writer
+                writer = csv.writer(file)
+                # Agregar la fila de información
+                sku = input("Inserte SKU: ")
+                nombre = input("Inserte Nombre: ")
+                while True:
+                    print("Inserte que tipo de unidad quiere")
+                    print("")
+                    print("0.- Pieza")
+                    print("1.- Paquete")
+                    valorunidad = input("")
+                    if valorunidad == "0":
+                        unidad = "pieza"
+                        fila = [sku, nombre, unidad]
+                        writer.writerow(fila)
+                        print("El producto a sido añadido correctamente")
+                        return False
+                    elif valorunidad == "1":
+                        unidad = "paquete"
+                        fila = [sku, nombre, unidad]
+                        writer.writerow(fila)
+                        print("El producto a sido añadido correctamente")
+                        return False
+                    else:
+                        print("Insete una opcion valida")
+                        print("")
+                
+        except Exception as e: # Atrapa cualquier excepcion
+                print(f"Error insertarProducto() :{e.args}") # Muestra en consola el error que ocurrio
+                return False # Regresa False si ocurrio un error en el metodo
 
     def buscarProductos(self): # Metodo para buscar un producto
         # TODO programar el método buscarProducto()

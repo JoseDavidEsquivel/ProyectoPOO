837aabd0f57558f619127d9c38944a289c6da558
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-03-19 18:19:36 -0600

    Update main.py
    Agregación de la función "Salir programa" y correcciones en los métodos

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from productos import Productos # Importa el modulo Productos
-from tiendas import Tiendas # Importa el modulo Tiendas
+
 #TODO importar Despensa
 
 class Main(): # Clase principal
@@ -16,25 +16,28 @@ class Main(): # Clase principal
             print("3.- Actualizar producto") # Opcion para actualizar un producto
             print("4.- Borrar producto") # Opcion para borrar un producto
             print("s.- Salir") # Opcion para salir del sistema
+            print("") # Espacio de tolerancia
             opcion = input("Seleccione una opcion: ") # Solicita al usuario que seleccione una opcion del menu
             if opcion == "0": # Valida si la opcion elegida es el 0
                 productos.listarProductos() # Llama al metodo listarProductos a traves del objeto productos
-            elif opcion == "1": # Valida si la opcion elegida es el 1
-                # TODO Llamar al metodo insertarProducto()
+            elif opcion == "1": # Valida si la opcion elegida es el 1              
                 productos.insertarProducto() # Llama al metodo insertarProductos a traves del objeto productos
-            elif opcion == "2": # Valida si la opcion elegida es el 2
-                # TODO Llamar al metodo buscarProducto()
-                 productos.buscarProdcto() # Llama al metodo buscarProductos a traves del objeto productos
+            elif opcion == "2": # Valida si la opcion elegida es el 2            
+                productos.buscarProducto() # Llama al metodo buscarProductos a traves del objeto productos
             elif opcion == "3": # Valida si la opcion elegida es el 3
-                # TODO Llamar al metodo actualizarProducto()
-                productos.actualizarProdcto() # Llama al metodo actualizarProductos a traves del objeto productos
+                productos.actualizarProducto() # Llama al metodo actualizarProductos a traves del objeto productos
             elif opcion == "4": # Valida si la opcion elegida es el 4
-                  productos.borrarProductos() #Llama al metodo borrarProductos a traves del objeto productos 
-                # TODO Llamar al metodo borrarProducto()
-                print("Llamar metodo borrarProducto()")
-            elif opcion == "5": # Valida si la opcion elegida es el 5
-                # TODO Programar salir del programa
-                print("Salir del programa")
+                productos.borrarProducto() #Llama al metodo borrarProductos a traves del objeto productos 
+            elif opcion == "s": # Valida si la opcion elegida es el 5
+                print("")
+                print("Saliendo del programa")
+                print("")
+                return False
+            elif opcion == "S": # Valida si la opcion elegida es el 5
+                print("")
+                print("Saliendo del programa")
+                print("")
+                return False
 
             # TODO rediseñar el menu para agregar los submenus necesarios
 

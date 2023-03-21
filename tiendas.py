f80f9d924a75392e320943f9de858f3f35118711
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-03-20 20:08:17 -0600

    Update tiendas.py
    se agrego el código de buscar tiendas

--- tiendas.py
+++ tiendas.py
@@ -14,9 +14,29 @@ class Tiendas:  # Clase Tiendas
         # TODO programar el método insertarTienda()
         return False  # Regresa False si ocurrio un error en el metodo
 
-    def buscarTiendas(self, nombre: str) -> bool:  # Metodo para buscar tiendas por nombre
-        # TODO programar el método buscarTienda()
-        return False  # Regresa False si ocurrio un error en el metodo
+ def buscarTiendas(self) -> bool: # Metodo para buscar tiendas
+        try: # Prueba el codigo y si ocurre una Excepcion la atrapa
+            with open("tiendas.csv", "r") as file: # Abre el archivo para poder leer filas de informacion
+                reader = csv.DictReader(file, delimiter=",") # Crer un objeto reader para leer los registros separandolos por el delimitador ,
+
+                id_buscar = input("Ingrese el id de la tienda que desea buscar: ") # Pide al usuario el id a buscar
+                encontrado = False # Variable para indicar si se encontró la tienda o no
+
+                for row in reader: # Recorre todos los registros encontrados y almacena temporalmente cada uno en row
+                    if row['ID'] == id_buscar: # Compara el id buscando
+                        print(f"Registro encontrado: {row}") # Imprime la fila encontrada
+                        encontrado = True # Marca la tienda como encontrada
+                        break # Termina el bucle ya que se encontró el producto
+
+                if not encontrado: # If por si no exista el sku
+                    print("Tienda no encontrada") # Imprime el mensaje si la tienda no se encontró
+
+            return True # Retorna True si el método se ejecutó correctamente
+
+        except Exception as e: # Atrapa cualquier excepcion
+            print(f"Error buscarTienda(): {e.args}") # Muestra en consola el error que ocurrio
+            return False  # Regresa False si ocurrio un error en el metodo
+
 
     def borrarTiendas(self, direccion: str) -> bool:  # Metodo para borrar tiendas por direccion
         with open('tiendas.csv', 'r', newline='') as archivo, open('temp.csv', 'w', newline='') as archivo_temp:

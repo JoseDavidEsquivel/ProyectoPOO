6cc70749668d992e99b8505a210ac6ec83123451
Eros Yael Castañeda Téllez <122810314+ErosCasta@users.noreply.github.com>
2023-03-28 13:48:33 -0600

    Update despensa.py
    Se coloco el código de listar despensa

--- despensa.py
+++ despensa.py
@@ -6,10 +6,16 @@ class Despensa:  # Clase Despensa
     def __init__(self): # Constructor de la clase
         pass # Inicializa el objeto
 
-    def listarDespensa(self, fecha: str) -> bool: # Metodo para listarDespensa()
-        # TODO programar el método listarDespensa()
-        # mostrar los registros por la fecha indicada con los siguientes datos:
-        # sku, nombre del producto, nombre de la tienda
+    def listarDespensa(self) -> bool: # Metodo para listar compras registradas
+        try: # Prueba el codigo y si ocurre una Excepcion la atrapa
+            with open("despensa.csv", "r") as file: # Abre el archivo para tener acceso a los registros
+                reader = csv.DictReader(file, delimiter=",") # Crer un objeto reader para leer los registros separandolos por el delimitador ,
+                for row in reader: # Recorre todos los registros encontrados y almacena temporalmente cada uno en row
+                    print(f"Registro: {row}") # imprime los datos del registro como un diccionario
+            return True # Regresa True si el metodo se ejecuto correctamente
+        except Exception as e: # Atrapa cualquier excepcion
+            print(f"Error listarProductos() :{e.args}") # Muestra en consola el error que ocurrio
+            return False # Regresa False si ocurrio un error en el metodo
         return False # Regresa False si ocurrio un error en el metodo # Regresa False si ocurrio un error en el metodo
 
     def insertarDespensa(self) -> bool: # Metodo para insertarDespesa()

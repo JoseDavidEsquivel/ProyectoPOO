96cc7969cfa22b031e15e345623c81de1ac82731
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-04-12 21:20:09 -0600

    Update maindespensa.py
    Agregacion de los metodos de mayor y menor precio al menu de despensa

--- maindespensa.py
+++ maindespensa.py
@@ -16,30 +16,36 @@ class MainDespensa(): # Clase principal
             print("2.- Buscar despensa por Fecha") # Opcion para buscar despensa por fecha
             print("3.- Actualizar despensa") # Opcion para actualizar una despensa
             print("4.- Borrar despensa") # Opcion para borrar una despensa
+            print("5.- Valor Minimo de un Producto") # Opcion para valor minimo de un producto
+            print("6.- Valor Maximo de un Producto") # Opcion para valor maximo de un producto
             print("s.- Salir") # Opcion para salir del sistema
             print("") # Espacio de tolerancia
             opcion = input("Seleccione una opcion: ") # Solicita al usuario que seleccione una opcion del menu
             if opcion == "0": # Valida si la opcion elegida es el 0
-                despensa.listarDespensa() # Llama al metodo listarProductos a traves del objeto productos
+                despensa.listarDespensa() # Llama al metodo listarDespensa a traves del objeto Despensa
             elif opcion == "1": # Valida si la opcion elegida es el 1              
                 obtener_ultimo_id_compra()
-                despensa.insertarDespensa() # Llama al metodo insertarProductos a traves del objeto productos
+                despensa.insertarDespensa() # Llama al metodo insertarDespensa a traves del objeto Despensa
             elif opcion == "2": # Valida si la opcion elegida es el 2            
-                despensa.buscarDespensa() # Llama al metodo buscarProductos a traves del objeto productos
+                despensa.buscarDespensa() # Llama al metodo buscarDespensa a traves del objeto Despensa
             elif opcion == "3": # Valida si la opcion elegida es el 3
                 obtener_ultimo_id_compra()
-                despensa.actualizarDespensa() # Llama al metodo actualizarProductos a traves del objeto productos
+                despensa.actualizarDespensa() # Llama al metodo actualizarDespensa a traves del objeto Despensa
             elif opcion == "4": # Valida si la opcion elegida es el 4
-                despensa.borrarDespensa() #Llama al metodo borrarProductos a traves del objeto productos 
-            elif opcion == "s": # Valida si la opcion elegida es el 5
-                print("")
-                print("Saliendo del programa")
-                print("")
+                despensa.borrarDespensa() #Llama al metodo borrarDespensa a traves del objeto Despensa
+            elif opcion == "5": # Valida si la opcion elegida es el 5
+                despensa.valorMinimoProducto() #Llama al metodo valorMinimoProducto a traves del objeto Despensa
+            elif opcion == "6": # Valida si la opcion elegida es el 6
+                despensa.valorMaximoProducto() #Llama al metodo valorMaximoProducto a traves del objeto Despensa
+            elif opcion == "s": # Valida si la opcion elegida es el s
+                print("") # Espacio de tolerancia
+                print("Saliendo del menu") # Avisa que esta saliendo del menu
+                print("") # Espacio de tolerancia
                 return False
-            elif opcion == "S": # Valida si la opcion elegida es el 5
-                print("")
-                print("Saliendo del programa")
-                print("")
+            elif opcion == "S": # Valida si la opcion elegida es el S
+                print("") # Espacio de tolerancia
+                print("Saliendo del menu") # Avisa que esta saliendo del menu
+                print("") # Espacio de tolerancia
                 return False
 
             # TODO rediseñar el menu para agregar los submenus necesarios

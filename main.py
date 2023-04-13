fa958e7b899f9f738e48bb0a1192a82c55ec3cfa
JoseDavidEsquivel <122810644+JoseDavidEsquivel@users.noreply.github.com>
2023-04-12 21:01:13 -0600

    Update main.py
    agregacion del menu de la funcion inovadora

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from tiendas import Tiendas # Importa el modulo Tiendas
 from productos import Productos # Importa el modulo Productos
 from despensa import Despensa # Importa el modulo Despensas
+from rastreador import Rastreador # Importa el modulo Rastreador
 from maintiendas import MainTiendas # Importa el modulo MainTiendas
 from mainproductos import MainProductos # Importa el modulo MainProductos
 from maindespensa import MainDespensa # Importa el modulo MainDespensa
+from mainrastreador import MainRastreador # Importa el modulo MainRastreador
 
 #TODO importar Despensa
 
@@ -16,11 +18,13 @@ class Main(): # Clase principal
         principaltiendas = MainTiendas() # Crea un objeto de la clase Main Tiendas
         principalproductos = MainProductos() # Crea un objeto de la clase Main Productos
         principaldespensa = MainDespensa() # Crea un objeto de la clase Main Despensa
+        principalrastreador = MainRastreador() # Crea un objeto de la clase Main Rastreador
         
         while True: # Bucle infinito para mostrar las opciones del sistema
             print("0.- Ingresar al menu de Tiendas") # Menciona la opcion para entrar al menu de Tiendas
             print("1.- Ingresar al menu de Productos") # Menciona la opcion para entrar al menu de Producots
             print("2.- Ingresar al menu de Despensa") # Menciona la opcion para entrar al menu de Despensa
+            print("3.- Ingresar al menu de Rastrear") # Menciona la opcion para entrar al menu de Rastrear
             
             print("s.- Salir") # Opcion para salir del sistema
             print("") # Espacio de tolerancia
@@ -29,14 +33,16 @@ class Main(): # Clase principal
                 principaltiendas.menuTiendas() # Llama al metodo menu a traves del objeto principal
             elif opcion == "1": # Valida si la opcion elegida es el 1              
                 principalproductos.menuProductos() # Llama al metodo menu a traves del objeto principal
-            elif opcion == "2": # Valida si la opcion elegida es el 1              
+            elif opcion == "2": # Valida si la opcion elegida es el 2              
                 principaldespensa.menuDespensa() # Llama al metodo menu a traves del objeto principal
-            elif opcion == "s": # Valida si la opcion elegida es el 5
+            elif opcion == "3": # Valida si la opcion elegida es el 3              
+                principalrastreador.menuRastreador() # Llama al metodo menu a traves del objeto principal
+            elif opcion == "s": # Valida si la opcion elegida es el s
                 print("") # Espacio de tolerancia
                 print("Saliendo del programa") # Notificacion de salida de programa
                 print("") # Espacio de tolerancia
                 return False # Regresa un "False" para terminar con el bucle
-            elif opcion == "S": # Valida si la opcion elegida es el 5
+            elif opcion == "S": # Valida si la opcion elegida es el S
                 print("") # Espacio de tolerancia
                 print("Saliendo del programa") # Notificacion de salida de programa
                 print("") # Espacio de tolerancia
